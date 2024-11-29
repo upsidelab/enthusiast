d53da4c91cad3548616ad007078ba12b7a726781
Dawid Mularczyk <72767335+Davsooonowy@users.noreply.github.com>
2024-11-29 13:23:58 +0100

    feat: add development dockerfile

---
+++ server/pecl/docker-entrypoint-development.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+
+if [ "$RUN_WORKER" = "True" ]; then
+    exec celery -A pecl.celery worker
+else
+    if [ "$RUN_MIGRATIONS" = "True" ]; then
+        # TODO Make this a migration inside the server codebase
+        apt-get update && apt-get install -y postgresql-client
+        PGPASSWORD=$ECL_DB_PASSWORD psql -c "CREATE EXTENSION IF NOT EXISTS vector" -h $ECL_DB_HOST -p $ECL_DB_PORT -U $ECL_DB_USER $ECL_DB_NAME
+
+        python manage.py migrate
+    fi
+
+    exec python manage.py runserver 0.0.0.0:${PORT:-8000}
+fi

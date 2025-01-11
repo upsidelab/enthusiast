c6c5bfe1a14bed838d1ff60632c3c6c695dae1aa
Rafal Cymerys <rafal@upsidelab.io>
2025-01-11 20:51:59 +0100

    fix: OpenAI embeddings provider interface

--- plugins/enthusiast-model-openai/enthusiast_model_openai/embedding.py
+++ plugins/enthusiast-model-openai/enthusiast_model_openai/embedding.py
@@ -7,10 +7,6 @@ from enthusiast_model_openai import utils
 PRIORITIZED_MODELS = ["text-embedding-3-large", "text-embedding-3-small"]
 
 class OpenAIEmbeddingProvider(EmbeddingProvider):
-    def __init__(self):
-        super(EmbeddingProvider, self).__init__()
-        self._client = OpenAI()
-
     def generate_embeddings(self, content: str) -> list[float]:
         """
         Generates and returns an embedding vector for the given content using OpenAI's embeddings API.
@@ -18,9 +14,9 @@ class OpenAIEmbeddingProvider(EmbeddingProvider):
         Args:
             content (str): The input text for which the embedding vector is to be generated.
         """
-        openai_embedding = self._client.embeddings.create(model=self._model,
-                                                          dimensions=self._dimensions,
-                                                          input=content)
+        openai_embedding = OpenAI().embeddings.create(model=self._model,
+                                                      dimensions=self._dimensions,
+                                                      input=content)
 
         return openai_embedding.data[0].embedding
 

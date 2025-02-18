92a5aa5aec4762b79cc62489bab6ba824e4ec390
Rafal Cymerys <rafal@upsidelab.io>
2025-02-18 16:41:26 +0100

    WIP

--- server/agent/core/agent.py
+++ server/agent/core/agent.py
@@ -1,9 +1,10 @@
 import logging
 
 from langchain import hub
-from langchain.agents import create_react_agent, AgentExecutor
+from langchain.agents import create_react_agent, AgentExecutor, create_tool_calling_agent
 from langchain.memory import ConversationSummaryBufferMemory
 from langchain_core.messages import SystemMessage
+from langchain_core.prompts import ChatPromptTemplate
 
 from catalog.language_models import LanguageModelRegistry
 from catalog.models import DataSet
@@ -13,6 +14,10 @@ logger = logging.getLogger(__name__)
 
 DEFAULT_SYSTEM_PROMPT = "You are an agent that knows everything about company\'s product catalog and content"
 
+REACT_PROMPT = """
+
+"""
+
 class Agent:
     def __init__(self, data_set: DataSet, messages: list):
         logger.debug("Initialize Agent")
@@ -21,9 +26,19 @@ class Agent:
         self._tools = ToolManager(data_set=data_set, language_model_provider=language_model_provider).tools
         self._system_message = SystemMessage(DEFAULT_SYSTEM_PROMPT)
         base_prompt = hub.pull("langchain-ai/react-agent-template")
-        prompt = base_prompt.partial(instructions='You are an agent that knows everything about company\'s product catalog and content')
+        # prompt = base_prompt.partial(instructions='You are an agent that knows everything about company\'s product catalog and content')
 
-        self._agent = create_react_agent(self._llm, self._tools, prompt=prompt)
+        prompt = ChatPromptTemplate.from_messages(
+            [
+                ("system", "You are a helpful assistant tat knows everything about company's products."),
+                ("human", "{input}"),
+                # Placeholders fill up a **list** of messages
+                ("placeholder", "{agent_scratchpad}"),
+            ]
+        )
+
+        self._agent = create_tool_calling_agent(self._llm, self._tools, prompt)
+        # self._agent = create_react_agent(self._llm, self._tools, prompt=prompt)
         self._agent_executor = AgentExecutor(agent=self._agent, tools=self._tools, verbose=True)
         # Populate memory with history of conversation.
         # logger.debug(f"Populate memory. Number of messages in conversation history: {len(messages)}")

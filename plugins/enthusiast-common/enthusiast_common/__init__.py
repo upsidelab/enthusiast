c51a3688b6df9b5845a423fa88a71d2ae7e93f15
dsowinski2 <48838969+dsowinski2@users.noreply.github.com>
2025-06-06 08:29:29 +0200

    feat: Add Google models plugin (#59)

--- plugins/enthusiast-common/enthusiast_common/__init__.py
+++ plugins/enthusiast-common/enthusiast_common/__init__.py
@@ -1,6 +1,7 @@
-from .interfaces import DocumentSourcePlugin as DocumentSourcePlugin
-from .interfaces import ProductSourcePlugin as ProductSourcePlugin
-from .interfaces import EmbeddingProvider as EmbeddingProvider
 from .interfaces import CustomTool as CustomTool
+from .interfaces import DocumentSourcePlugin as DocumentSourcePlugin
+from .interfaces import EmbeddingProvider as EmbeddingProvider
+from .interfaces import LanguageModelProvider as LanguageModelProvider
+from .interfaces import ProductSourcePlugin as ProductSourcePlugin
 from .structures import DocumentDetails as DocumentDetails
 from .structures import ProductDetails as ProductDetails
--- plugins/enthusiast-model-openai/enthusiast_model_openai/language_model.py
+++ plugins/enthusiast-model-openai/enthusiast_model_openai/language_model.py
@@ -1,15 +1,16 @@
-from enthusiast_common.interfaces import LanguageModelProvider
+from enthusiast_common import LanguageModelProvider
+from enthusiast_common.utils import prioritize_items
 from langchain_core.language_models import BaseLanguageModel
 from langchain_openai import ChatOpenAI
 from openai import OpenAI
 
-from enthusiast_model_openai import utils
 
 PRIORITIZED_MODELS = ["gpt-4o", "gpt-4o-mini"]
 
+
 class OpenAILanguageModelProvider(LanguageModelProvider):
     def provide_language_model(self) -> BaseLanguageModel:
-        return ChatOpenAI(name=self._model)
+        return ChatOpenAI(model=self._model)
 
     def model_name(self) -> str:
         return self._model
@@ -18,4 +19,4 @@ class OpenAILanguageModelProvider(LanguageModelProvider):
     def available_models() -> list[str]:
         all_models = OpenAI().models.list().data
         gpt_models = [model.id for model in all_models if model.id.startswith("gpt-")]
-        return utils.prioritize_items(gpt_models, PRIORITIZED_MODELS)
+        return prioritize_items(gpt_models, PRIORITIZED_MODELS)

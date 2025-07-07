77fbc4d60ef5afb5df6d8b5fa4bf632bc695c84f
Rafal Cymerys <rafal@upsidelab.io>
2025-07-07 15:23:32 +0200

    feat: fix scroll and add a new chat button

--- frontend/src/pages/data-sets/(id)/chat/(id)/index.tsx
+++ frontend/src/pages/data-sets/(id)/chat/(id)/index.tsx
@@ -1,20 +1,27 @@
 import { Conversation } from "@/components/conversation-view/conversation.tsx";
 import { Conversation as ConversationSchema } from '@/lib/types.ts';
-import { useParams, useSearchParams } from "react-router-dom";
+import { useNavigate, useParams, useSearchParams } from "react-router-dom";
 import { PageHeading } from "@/components/util/page-heading.tsx";
 import { PageMain } from "@/components/util/page-main.tsx";
 import { ApiClient } from "@/lib/api.ts";
 import { authenticationProviderInstance } from "@/lib/authentication-provider.ts";
 import { useEffect, useState } from "react";
+import { Button } from "@/components/ui/button.tsx";
+import { useApplicationContext } from "@/lib/use-application-context.ts";
+import { PlusIcon } from "lucide-react";
 
 const api = new ApiClient(authenticationProviderInstance);
 
 export function Chat() {
   const { chatId } = useParams();
+  const { dataSetId } = useApplicationContext()!;
   const [searchParams, setSearchParams] = useSearchParams();
   const conversationId = Number(chatId);
   const pendingMessage = searchParams.get("pending");
   const [conversation, setConversation] = useState<ConversationSchema | null>(null);
+  const navigate = useNavigate();
+
+  const agentOrDefault = (conversation ? conversation.agent : null) || 'Question Answer Agent';
 
   const onPendingMessageSent = () => {
     setSearchParams({});
@@ -29,10 +36,15 @@ export function Chat() {
   }, [conversationId]);
 
   return (
-    <PageMain className="h-full">
+    <PageMain className="h-full py-0">
       {conversation &&
         <>
-          <PageHeading title={pendingMessage || conversation.summary!} description={`Chat with ${conversation.agent}`} className="sticky top-0 p-4 bg-white" />
+          <PageHeading title={pendingMessage || conversation.summary!} description={`Chat with ${conversation.agent}`} className="sticky top-0 pt-4 bg-white">
+            <Button className="ml-auto mr-0" variant="outline" onClick={() => navigate(`/data-sets/${dataSetId}/chat/new/${encodeURIComponent(agentOrDefault)}`)}>
+              <PlusIcon />
+              New Chat
+            </Button>
+          </PageHeading>
           <Conversation conversationId={conversationId} onPendingMessageSent={onPendingMessageSent} pendingMessage={pendingMessage} />
         </>
       }

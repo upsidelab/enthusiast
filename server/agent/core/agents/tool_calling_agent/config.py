5851626e2d7e6c3723216d22e3ad875ac9b74c9d
dsowinski2 <48838969+dsowinski2@users.noreply.github.com>
2025-07-15 16:09:07 +0200

    feat: Add default config (#105)

--- server/agent/core/agents/tool_calling_agent/config.py
+++ server/agent/core/agents/tool_calling_agent/config.py
@@ -1,40 +1,15 @@
-from enthusiast_common.config import (
-    EmbeddingsRegistryConfig,
-    LLMConfig,
-    LLMRegistryConfig,
-    LLMToolConfig,
-    ModelsRegistryConfig,
-    RegistryConfig,
-    RepositoriesConfig,
-    RetrieverConfig,
-    RetrieversConfig,
-    ToolCallingAgentConfig,
-)
+from enthusiast_common.config import AgentConfig, AgentConfigWithDefaults, LLMConfig, LLMToolConfig
 from langchain_core.prompts import ChatPromptTemplate
 
 from agent.callbacks import ConversationWebSocketCallbackHandler
 from agent.core.agents import ToolCallingAgent
-from agent.injector import Injector
+from agent.core.agents.default_config import merge_config
 from agent.models import Conversation
-from agent.registries.embeddings import EmbeddingProviderRegistry
-from agent.registries.language_models import LanguageModelRegistry
-from agent.registries.models import BaseDjangoSettingsDBModelRegistry
-from agent.repositories import (
-    DjangoConversationRepository,
-    DjangoDataSetRepository,
-    DjangoDocumentChunkRepository,
-    DjangoMessageRepository,
-    DjangoProductChunkRepository,
-    DjangoProductRepository,
-    DjangoUserRepository,
-)
-from agent.retrievers import DocumentRetriever, ProductRetriever
-from agent.retrievers.product_retriever import QUERY_PROMPT_TEMPLATE
 from agent.tools import CreateAnswerTool
 
 
-def get_config(conversation: Conversation, streaming: bool) -> ToolCallingAgentConfig:
-    return ToolCallingAgentConfig(
+def get_config(conversation: Conversation, streaming: bool) -> AgentConfig:
+    base_config = AgentConfigWithDefaults(
         conversation_id=conversation.id,
         prompt_template=ChatPromptTemplate.from_messages(
             [
@@ -54,30 +29,5 @@ def get_config(conversation: Conversation, streaming: bool) -> ToolCallingAgentC
             )
         ],
         llm=LLMConfig(callbacks=[ConversationWebSocketCallbackHandler(conversation.id)], streaming=streaming),
-        injector=Injector,
-        repositories=RepositoriesConfig(
-            user=DjangoUserRepository,
-            data_set=DjangoDataSetRepository,
-            conversation=DjangoConversationRepository,
-            message=DjangoMessageRepository,
-            product=DjangoProductRepository,
-            document_chunk=DjangoDocumentChunkRepository,
-            product_chunk=DjangoProductChunkRepository,
-        ),
-        retrievers=RetrieversConfig(
-            document=RetrieverConfig(retriever_class=DocumentRetriever),
-            product=RetrieverConfig(
-                retriever_class=ProductRetriever,
-                extra_kwargs={
-                    "prompt_template": QUERY_PROMPT_TEMPLATE,
-                    "max_sample_products": 12,
-                    "number_of_products": 12,
-                },
-            ),
-        ),
-        registry=RegistryConfig(
-            llm=LLMRegistryConfig(registry_class=LanguageModelRegistry),
-            embeddings=EmbeddingsRegistryConfig(registry_class=EmbeddingProviderRegistry),
-            model=ModelsRegistryConfig(registry_class=BaseDjangoSettingsDBModelRegistry),
-        ),
     )
+    return merge_config(base_config)

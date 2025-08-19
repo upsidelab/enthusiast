fb1b9de7e86c7eeea6b1e15e056cf462456d0b35
Damian Sowiński <dsowinski2@gmail.com>
2025-08-19 08:18:41 +0200

    chore: Remove product search agent

--- server/agent/core/retrievers/__init__.py
+++ server/agent/core/retrievers/__init__.py
@@ -1,9 +1,7 @@
 from .document_retriever import DocumentRetriever
 from .product_retriever import ProductRetriever
-from .product_vs_retriever import ProductVectorStoreRetriever
 
 __all__ = [
     "DocumentRetriever",
     "ProductRetriever",
-    "ProductVectorStoreRetriever",
 ]
--- server/agent/core/tools/__init__.py
+++ server/agent/core/tools/__init__.py
@@ -1,11 +1,5 @@
 from .create_answer_tool import CreateAnswerTool
-from .product_search_tool import ProductsSearchTool
-from .product_vs_search_tool import ProductVectorStoreSearchTool
-from .verify_product_tool import ProductVerificationTool
 
 __all__ = [
     "CreateAnswerTool",
-    "ProductVerificationTool",
-    "ProductsSearchTool",
-    "ProductVectorStoreSearchTool",
 ]

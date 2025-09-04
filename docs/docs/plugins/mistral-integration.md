b1f9f07ff535c0118fb5079f0996ecc2298ed255
dsowinski2 <48838969+dsowinski2@users.noreply.github.com>
2025-09-04 13:00:10 +0200

    feat: Add Azure OpenAI model plugin (#171)

---
+++ docs/docs/plugins/mistral-integration.md
@@ -0,0 +1,33 @@
+---
+sidebar_position: 6
+---
+
+# MistralAI Integration
+
+The MistralAI plugin for Enthusiast, makes it possible to use MistralAI models.
+
+## Installing the Plugin
+
+First, install the `enthusiast-model-mistral` package using pip:
+
+```shell
+potery add enthusiast-model-mistral
+```
+
+Then, enable the relevant language model and embedding providers in the `settings.py`:
+
+```python title="server/pecl/settings.py"
+CATALOG_LANGUAGE_MODEL_PROVIDERS = {
+    "Mistral": "enthusiast_model_mistral.MistralAILanguageModelProvider",
+}
+
+CATALOG_EMBEDDING_PROVIDERS = {
+    "Mistral": "enthusiast_model_mistral.MistralAIEmbeddingProvider",
+}
+```
+
+Save the changes and restart the web server and the worker.
+
+## Configuring a Data Set to use Azure OpenAI
+
+After installing and configuring the plugin, you can now select Mistral as a language model and embedding provider when [setting up a new data set](/tools/enthusiast/docs/synchronize/manage-data-sets/#creating-a-data-set).
---
+++ plugins/enthusiast-model-azureopenai/enthusiast_model_openai/language_model.py
@@ -0,0 +1,25 @@
+from enthusiast_common.registry.llm import LanguageModelProvider
+from enthusiast_common.utils import prioritize_items
+from langchain_core.callbacks import BaseCallbackHandler
+from langchain_core.language_models import BaseLanguageModel
+from langchain_openai import AzureChatOpenAI
+from openai import AzureOpenAI
+
+PRIORITIZED_MODELS = ["gpt-4o", "gpt-4.1", "gpt-4o-mini", "gpt-4.1-mini"]
+
+
+class AzureOpenAILanguageModelProvider(LanguageModelProvider):
+    def provide_language_model(self, callbacks: list[BaseCallbackHandler] | None = None) -> BaseLanguageModel:
+        return AzureChatOpenAI(model=self._model, callbacks=callbacks)
+
+    def provide_streaming_language_model(self, callbacks: list[BaseCallbackHandler] | None) -> BaseLanguageModel:
+        return AzureChatOpenAI(model=self._model, callbacks=callbacks, streaming=True)
+
+    def model_name(self) -> str:
+        return self._model
+
+    @staticmethod
+    def available_models() -> list[str]:
+        all_models = AzureOpenAI().models.list().data
+        gpt_models = [model.id for model in all_models if model.id.startswith("gpt-")]
+        return prioritize_items(gpt_models, PRIORITIZED_MODELS)

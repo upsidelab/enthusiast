43b7782db2065508177bdf9e47e3c863e480d243
Damian Sowiński <dsowinski2@gmail.com>
2025-09-19 08:54:54 +0200

    feat: Handle file preparation according to choosen model in Ollama provider

--- plugins/enthusiast-model-ollama/enthusiast_model_ollama/language_model.py
+++ plugins/enthusiast-model-ollama/enthusiast_model_ollama/language_model.py
@@ -1,12 +1,124 @@
+from typing import Callable
+
+from enthusiast_common.config.prompts import BaseContent
 from enthusiast_common.registry.llm import LanguageModelProvider
+from enthusiast_common.structures import LLMFile
 from langchain_core.callbacks import BaseCallbackHandler
 from langchain_core.language_models import BaseLanguageModel
 from langchain_ollama import ChatOllama
 from ollama import Client
+from pydantic import ConfigDict
+
+
+class OllamaFileObject(BaseContent):
+    model_config = ConfigDict(extra="allow")
+
+
+class OllamaPrepareFileMapper:
+    @staticmethod
+    def gpt_image(file_object: LLMFile, data_placeholder: bool = True) -> OllamaFileObject:
+        if data_placeholder:
+            image_url = (
+                f'data:{file_object.content_type};base64,"{{{LanguageModelProvider.FILE_KEY_PREFIX}{file_object.id}}}"'
+            )
+        else:
+            image_url = f'data:{file_object.content_type};base64,"{file_object.content}"'
+        return OllamaFileObject(
+            type="input_image",
+            image_url=image_url,
+        )
+
+    @staticmethod
+    def gpt_file(file_object: LLMFile, data_placeholder: bool = True) -> OllamaFileObject:
+        if data_placeholder:
+            file_data = f'"{{{LanguageModelProvider.FILE_KEY_PREFIX}{file_object.id}}}"'
+        else:
+            file_data = file_object.content
+        return OllamaFileObject(
+            type="input_file",
+            file_data=file_data,
+            filename=file_object.filename,
+        )
+
+    @staticmethod
+    def mistral_image(file_object: LLMFile, data_placeholder: bool = True) -> OllamaFileObject:
+        if data_placeholder:
+            image_url = (
+                f"data:{file_object.content_type};base64,{{{LanguageModelProvider.FILE_KEY_PREFIX}{file_object.id}}}"
+            )
+        else:
+            image_url = f"data:{file_object.content_type};base64,{file_object.content}"
+        return OllamaFileObject(
+            type="image_url",
+            image_url=image_url,
+        )
+
+    @staticmethod
+    def mistral_file(file_object: LLMFile, data_placeholder: bool = True) -> OllamaFileObject:
+        if data_placeholder:
+            document_url = (
+                f"data:{file_object.content_type};base64,{{{LanguageModelProvider.FILE_KEY_PREFIX}{file_object.id}}}"
+            )
+        else:
+            document_url = f"data:{file_object.content_type};base64,{file_object.content}"
+        return OllamaFileObject(
+            type="document_url",
+            document_url=document_url,
+        )
+
+    @staticmethod
+    def gemini_image(file_object: LLMFile, data_placeholder: bool = True) -> OllamaFileObject:
+        if data_placeholder:
+            image_url = (
+                f"data:{file_object.content_type};base64,{{{LanguageModelProvider.FILE_KEY_PREFIX}{file_object.id}}}"
+            )
+        else:
+            image_url = f"data:{file_object.content_type};base64,{file_object.content}"
+        return OllamaFileObject(
+            type="image_url",
+            image_url=image_url,
+        )
+
+    @staticmethod
+    def gemini_file(file_object: LLMFile, data_placeholder: bool = True) -> OllamaFileObject:
+        if data_placeholder:
+            data = f"{LanguageModelProvider.FILE_KEY_PREFIX}{file_object.id}"
+        else:
+            data = file_object.content
+        return OllamaFileObject(
+            type="input_file",
+            data=data,
+            mime_type=file_object.content_type,
+        )
+
+    MODEL_MAPPING: dict[str, dict[str, Callable[[LLMFile, bool], OllamaFileObject]]] = {
+        "gpt": {
+            "image": gpt_image.__func__,
+            "file": gpt_file.__func__,
+        },
+        "mistral": {
+            "image": mistral_image.__func__,
+            "file": mistral_file.__func__,
+        },
+        "models/gemini": {
+            "image": gemini_image.__func__,
+            "file": gemini_file.__func__,
+        },
+    }
+
+    def get_prepare_file_function(self, model: str, file_type: str) -> Callable[[LLMFile, bool], OllamaFileObject]:
+        model_object = self.MODEL_MAPPING.get(model)
+        if not model_object:
+            raise ValueError(f"Model {model} not supported.")
+        prepare_file_function = model_object.get(file_type)
+        if not prepare_file_function:
+            raise ValueError(f"Invalid file type {file_type} for model {model}.")
+        return prepare_file_function
 
 
 class OllamaLanguageModelProvider(LanguageModelProvider):
     STREAMING_AVAILABLE = False
+    MODEL_MAPPING_CLASS = OllamaPrepareFileMapper
 
     def provide_language_model(self, callbacks: list[BaseCallbackHandler] | None = None) -> BaseLanguageModel:
         return ChatOllama(model=self._model)
@@ -18,3 +130,13 @@ class OllamaLanguageModelProvider(LanguageModelProvider):
     def available_models() -> list[str]:
         ollama_client = Client()
         return [model.model for model in ollama_client.list().models]
+
+    def prepare_image_object(self, file_object: LLMFile, data_placeholder: bool = True) -> OllamaFileObject:
+        return self.MODEL_MAPPING_CLASS().get_prepare_file_function(model=self._model, file_type="image")(
+            file_object, data_placeholder
+        )
+
+    def prepare_file_object(self, file_object: LLMFile, data_placeholder: bool = True) -> OllamaFileObject:
+        return self.MODEL_MAPPING_CLASS().get_prepare_file_function(model=self._model, file_type="file")(
+            file_object, data_placeholder
+        )

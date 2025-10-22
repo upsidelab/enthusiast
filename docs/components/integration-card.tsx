bab620a435ab889e3fbf024cba4376ef19f6207b
dsowinski2 <48838969+dsowinski2@users.noreply.github.com>
2025-10-22 16:04:27 +0200

    docs: Make agent plugin image optional (#206)

--- docs/components/integration-card.tsx
+++ docs/components/integration-card.tsx
@@ -3,14 +3,14 @@ import Image from "next/image";
 
 export interface IntegrationCardProps {
   name: string;
-  imageSrc: string;
+  imageSrc?: string;
   href: string;
 }
 
 export default function IntegrationCard({ name, imageSrc, href }: IntegrationCardProps) {
   return (
     <Cards.Card title={name} href={href}>
-      <Image className="x:p-4" src={imageSrc} alt={name} width={196} height={196} />
+      {imageSrc && (<Image className="x:p-4" src={imageSrc} alt={name} width={196} height={196}/>)}
     </Cards.Card>
   )
 }

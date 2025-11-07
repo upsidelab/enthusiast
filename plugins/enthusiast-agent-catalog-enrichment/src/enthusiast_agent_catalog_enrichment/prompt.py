cd5d02cb8a00c3d275ded5b13a44ced46bba70e2
Rafał Cymerys <rafal@upsidelab.io>
2025-11-07 11:23:41 +0100

    update prompt

--- plugins/enthusiast-agent-catalog-enrichment/src/enthusiast_agent_catalog_enrichment/prompt.py
+++ plugins/enthusiast-agent-catalog-enrichment/src/enthusiast_agent_catalog_enrichment/prompt.py
@@ -1,5 +1,5 @@
 DATA_EXTRACTION_AGENT_PROMPT = """
-I want you to help extracting and describing in details measurements for products using the ReACT (Reasoning and Acting) approach.
+I want you to help extracting and describing in details attributes for products using the ReACT (Reasoning and Acting) approach.
 In case of any missing information carefully collect it one by one.
 In tools specify exactly what are you looking for.
 If there are more variants return all of them.

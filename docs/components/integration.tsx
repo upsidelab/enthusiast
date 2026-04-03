2991f645cf4155695053da697d79fd78e974accd
mateus-po <mateusz.porebski99@gmail.com>
2026-04-03 13:36:08 +0200

    fix deprecated settings format references in docs - for LLM modules and embeddings modules

--- docs/components/integration.tsx
+++ docs/components/integration.tsx
@@ -25,9 +25,6 @@ const P = mdxComponents.p;
 
 export default async function Integration(props: IntegrationProps) {
   const installationInstructions = await compileMdx(`\`\`\`bash\npoetry add ${props.pipName}\n\`\`\``);
-  const buildRegisterDictInstructionMd = (key: string, module: string) => {
-    return `\`\`\`python\n${key} = {\n    ...\n    "${props.name}": "${module}",\n}\n\`\`\``;
-  }
   const buildRegisterListInstructionMd = (key: string, module: string) => {
     return `\`\`\`python\n${key} = [\n    ...\n    "${module}",\n]\n\`\`\``;
   }
@@ -67,8 +64,8 @@ export default async function Integration(props: IntegrationProps) {
       {props.registerProductModule && <MDXRemote compiledSource={await compileMdx(buildRegisterListInstructionMd("CATALOG_PRODUCT_SOURCE_PLUGINS", props.registerProductModule))}/>}
       {props.registerDocumentModule && <MDXRemote compiledSource={await compileMdx(buildRegisterListInstructionMd("CATALOG_DOCUMENT_SOURCE_PLUGINS", props.registerDocumentModule))}/>}
       {props.registerECommerceIntegrationModule && <MDXRemote compiledSource={await compileMdx(buildRegisterListInstructionMd("CATALOG_ECOMMERCE_INTEGRATION_PLUGINS", props.registerECommerceIntegrationModule))}/>}
-      {props.registerLanguageModelModule && <MDXRemote compiledSource={await compileMdx(buildRegisterDictInstructionMd("CATALOG_LANGUAGE_MODEL_PROVIDERS", props.registerLanguageModelModule))}/>}
-      {props.registerEmbeddingsModule && <MDXRemote compiledSource={await compileMdx(buildRegisterDictInstructionMd("CATALOG_EMBEDDING_PROVIDERS", props.registerEmbeddingsModule))}/>}
+      {props.registerLanguageModelModule && <MDXRemote compiledSource={await compileMdx(buildRegisterListInstructionMd("CATALOG_LANGUAGE_MODEL_PROVIDERS", props.registerLanguageModelModule))}/>}
+      {props.registerEmbeddingsModule && <MDXRemote compiledSource={await compileMdx(buildRegisterListInstructionMd("CATALOG_EMBEDDING_PROVIDERS", props.registerEmbeddingsModule))}/>}
       <P>Then, restart your server for the newly installed plugin to become available.</P>
       <P>Finally, sign in to Enthusiast's Admin UI, go to the Integrations tab and configure the required credentials.</P>
     </>

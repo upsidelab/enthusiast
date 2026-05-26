6c368e750c0238517ee491bb1cca663cded924f3
Dawid Mularczyk <dawid.mularczyk@onet.pl>
2026-05-26 13:19:24 +0200

    refactor: remove stale CONFIGURATION_ARGS = None from FileListTool and FileRetrievalTool

--- plugins/enthusiast-agent-tools/src/enthusiast_agent_tools/tools/file_list_tool.py
+++ plugins/enthusiast-agent-tools/src/enthusiast_agent_tools/tools/file_list_tool.py
@@ -8,7 +8,6 @@ class FileListTool(BaseFileTool):
     DESCRIPTION = "It's tool for listing all available user's files/images. Use it always to determine file ID. Do not return ids to user use it only as tool input."
     ARGS_SCHEMA = None
     RETURN_DIRECT = False
-    CONFIGURATION_ARGS = None
 
     def run(self):
         files = self._injector.repositories.conversation.list_files(self._conversation_id)
